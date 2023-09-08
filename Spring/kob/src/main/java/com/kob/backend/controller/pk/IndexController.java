7248f4e2a5aa7574e0351247a9991dff05accdbf
成龙 王 <1310927505@qq.com>
2023-09-08 18:00:08 +0800

    first commit

---
+++ Spring/kob/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -0,0 +1,10 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/PK/")
+public class IndexController {
+
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+#### springboot项目练习
